6eafb1a27a7967c8b0ecb67e53132cab977d30a9
OwenTse <31269748+OwenTse@users.noreply.github.com>
2019-02-15 15:19:59 +0800

    Update Jira工时统计.user.js

--- Jira工时统计.user.js
+++ Jira工时统计.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jira工时统计
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://migujira.cmread.com/issues/?filter=*
@@ -12,6 +12,7 @@
 (function() {
     'use strict';
     var $ = $ || window.$;
+    var sum = 0;
     var count = 0;
     var tds;
     //customfield_10119 MDG
@@ -22,6 +23,7 @@
     }
     $("[title='排序 MDG工时']").get(0).innerHTML="MDG工时<br/>("+count+")";
     console.log("MDG工时"+count);
+    sum+=count;
     count = 0;
      //customfield_10120 用户
     tds = $(".customfield_10120");
@@ -31,6 +33,7 @@
     }
     $("[title='排序 用户工时']").get(0).innerHTML="用户工时<br/>("+count+")";
     console.log("用户工时"+count);
+    sum+=count;
     count = 0;
     //customfield_10121 交易
     tds = $(".customfield_10121");
@@ -40,6 +43,7 @@
     }
     $("[title='排序 交易工时']").get(0).innerHTML="交易工时<br/>("+count+")";
     console.log("交易工时"+count);
+    sum+=count;
     count = 0;
     //customfield_10122 内容
     tds = $(".customfield_10122");
@@ -49,6 +53,7 @@
     }
     $("[title='排序 内容工时']").get(0).innerHTML="内容工时<br/>("+count+")";
     console.log("内容工时"+count);
+    sum+=count;
     count = 0;
     //customfield_10124 营销
     tds = $(".customfield_10124");
@@ -58,6 +63,7 @@
     }
     $("[title='排序 营销工时']").get(0).innerHTML="营销工时<br/>("+count+")";
     console.log("营销工时"+count);
+    sum+=count;
     count = 0;
     //customfield_10123 互动
     tds = $(".customfield_10123");
@@ -67,6 +73,7 @@
     }
     $("[title='排序 互动工时']").get(0).innerHTML="互动工时<br/>("+count+")";
     console.log("互动工时"+count);
+    sum+=count;
     count = 0;
     //customfield_10125 公共
     tds = $(".customfield_10125");
@@ -76,5 +83,8 @@
     }
     $("[title='排序 公共工时']").get(0).innerHTML="公共工时<br/>("+count+")";
     console.log("公共工时"+count);
+    sum+=count;
+    $(".results-count-text").append("<span class='results-count-start'>&nbsp;&nbsp汇总工时"+sum+"&nbsp&nbsp</span>");
+    console.log("工时 汇总"+sum);
     // Your code here...
 })();
