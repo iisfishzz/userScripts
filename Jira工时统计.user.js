630aa5ec9a08d4d929b65b0e553466c3f5644c39
OwenTse <31269748+OwenTse@users.noreply.github.com>
2019-03-22 15:37:36 +0800

    增加总进度展示
    需要视图中增加进度列才能生效。

--- Jira工时统计.user.js
+++ Jira工时统计.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Jira工时统计
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.6
 // @description  try to take over the world!
 // @author       You
-// @match        http://migujira.cmread.com/issues/?filter=*
+// @match        http://migujira.cmread.com/issues/?*
 // @grant        none
 // ==/UserScript==
 
@@ -44,11 +44,11 @@
         cmr_cpsResources =  Number(db.getItem("cmr_cpsResources"));
     }
     //营销
-    var cmr_cpsResources = db.getItem("cmr_cpsResources");
-    if(cmr_cpsResources == null)
+    var cmr_mktResources = db.getItem("cmr_mktResources");
+    if(cmr_mktResources == null)
     {
-        db.setItem("cmr_cpsResources",19);
-        cmr_cpsResources =  Number(db.getItem("cmr_cpsResources"));
+        db.setItem("cmr_mktResources",19);
+        cmr_mktResources =  Number(db.getItem("cmr_mktResources"));
     }
     //互动
     var cmr_snsResources =  db.getItem("cmr_snsResources");
@@ -79,7 +79,7 @@
         cmr_ratio =  Number(db.getItem("cmr_ratio"));
     }
     //可承接工时
-    var kcj= Math.round(cmr_period*cmr_ratio*(cmr_mdgResources*1+cmr_usrResources*1+cmr_cpsResources*1+cmr_snsResources*1+cmr_opfResources*1+cmr_cpsResources*1+cmr_dealResources*1));
+    var kcj= Math.round(cmr_period*cmr_ratio*(cmr_mdgResources*1+cmr_usrResources*1+cmr_cpsResources*1+cmr_snsResources*1+cmr_opfResources*1+cmr_mktResources*1+cmr_dealResources*1));
     console.log("可承接"+kcj);
     //customfield_10119 MDG
     tds = $(".customfield_10119");
@@ -131,7 +131,7 @@
         for (i = 0; i < tds.size(); i++) {
             count = count + Number(tds.get(i).textContent.trim());
         }
-        $("[title='排序 营销工时']").get(0).innerHTML = "营销工时<br/>(" + count + ")("+Math.round(cmr_cpsResources*cmr_period*cmr_ratio)+")";
+        $("[title='排序 营销工时']").get(0).innerHTML = "营销工时<br/>(" + count + ")("+Math.round(cmr_mktResources*cmr_period*cmr_ratio)+")";
         console.log("营销工时" + count);
         sum += count;
         count = 0;
@@ -161,7 +161,62 @@
     $(".results-count-text").append("<span class='results-count-start'>&nbsp;&nbsp汇总工时 "+sum+"&nbsp&nbsp可承接："+kcj+"&nbsp&nbsp</span>");
     console.log("工时 汇总"+sum);
     }
-   
+    //aggregateprogress
+    var progressFieldTh = $("[data-id='aggregateprogress']");
+
+    if(progressFieldTh.length > 0) {
+        //hideOnPrint
+        var pgs = $(".hideOnPrint");
+        console.log(pgs);
+        //初始预估 - 3 天
+        var initialValue = 0 ;
+        //剩余的估算 - 3 天
+        var surplusValue = 0;
+        //耗费时间 - 1 小时
+        var costValue = 0;
+        var tmp = 0;
+       //耗费时间 - 2 周, 2 天, 11 分   剩余的估算 - 2 天, 7 小时, 59 分
+         for (i = 0; i < pgs.size(); i++) {
+            var pgtitle = pgs.get(i).title;
+              //console.log("1----"+i+"===="+pgtitle);
+             if(pgtitle.indexOf("初始预估")>=0){
+                 var gsstr = pgtitle.replace("初始预估 - ","")
+                               .replace(/周,?/,"*5*8*60+")
+                               .replace(/天,?/,"*8*60+")
+                               .replace(/小时,?/,"*60+")
+                               .replace("分","+");
+                 gsstr +="0"
+                //console.log("计算串"+gsstr);
+                initialValue+=eval(gsstr);
+                console.log(initialValue);
+             }
+             gsstr = "";
+             if(pgtitle.indexOf("剩余的估算")>=0){
+                gsstr = pgtitle.replace("剩余的估算 - ","")
+                               .replace(/周,?/,"*5*8*60+")
+                               .replace(/天,?/,"*8*60+")
+                               .replace(/小时,?/,"*60+")
+                               .replace("分","+");
+                 gsstr +="0"
+                //console.log("计算串"+gsstr);
+                surplusValue+=eval(gsstr);
+             }
+            if(pgtitle.indexOf("耗费时间")>=0){
+                gsstr = pgtitle.replace("耗费时间 - ","")
+                               .replace(/周,?/,"*5*8*60+")
+                               .replace(/天,?/,"*8*60+")
+                               .replace(/小时,?/,"*60+")
+                               .replace("分","+");
+                gsstr +="0"
+                //console.log("计算串"+gsstr);
+                costValue+=eval(gsstr);
+             }
+         }
+        var progress = Math.round(costValue/initialValue*100,2)+"%";
+        console.log("初始预估 "+initialValue+"剩余的估算 "+surplusValue+"耗费时间 "+costValue);
+        $(".results-count-text").append("<br/>总体时间进度"+Math.round(costValue/initialValue*100,2)+"%"
+                                       +"<br/>初始预估 "+Math.round(initialValue/60/8,2)+"天,剩余的估算"+Math.round(surplusValue/60/8,2)+"天,耗费时间"+Math.round(costValue/60/8,2)+"天,");
+    }
 
     // Your code here...
 })();
